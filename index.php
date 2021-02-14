1090162ebb03f36132b63237a2bf4dcfa25bd529
Jessica Sestak <jsestak2@mail.greenriver.edu>
2021-02-14 13:52:50 -0500

    Added home page

--- index.php
+++ index.php
@@ -12,9 +12,9 @@ $f3 = Base::instance();
 $f3->set('DEBUG', 3);
 
 $f3->route('GET /', function() {
-     echo "Hello world";
-   // $view = new Template();
- //   echo $view->render('views/home.html');
+    // echo "Hello world";
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 
