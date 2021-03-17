4cc1978c50049cf07bba9184bc7ef7016a2e24e9
Safal Adhikari <safal.adhikari1@outlook.com>
2021-03-17 00:18:54 -0400

    adds user class and premium user class

--- classes/users.php
+++ classes/users.php
@@ -2,12 +2,89 @@
 
 class Users
 {
-    private $_userId;
     private $_firstName;
     private $_lastName;
-    private $_isPetPrime;
-    private $_userPreference;
+    private $_email;
+    private $_userName;
+    private $_passWord;
 
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->_firstName;
+    }
 
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName($firstName)
+    {
+        $this->_firstName = $firstName;
+    }
 
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->_lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName)
+    {
+        $this->_lastName = $lastName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->_email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->_email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserName()
+    {
+        return $this->_userName;
+    }
+
+    /**
+     * @param mixed $userName
+     */
+    public function setUserName($userName)
+    {
+        $this->_userName = $userName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassWord()
+    {
+        return $this->_passWord;
+    }
+
+    /**
+     * @param mixed $passWord
+     */
+    public function setPassWord($passWord)
+    {
+        $this->_passWord = $passWord;
+    }
 }
--- model/data-layer.php
+++ model/data-layer.php
@@ -1,6 +1,139 @@
 <?php
 
+/*
+     CREATE TABLE primeusers (
+        userid int(5) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        fname varchar(20) NOT NULL,
+        lname varchar(20) NOT NULL,
+        email varchar(30) NOT NULL,
+        username varchar(20) NOT NULL,
+        password varchar(40) NOT NULL
+    );
+
+    INSERT INTO primeusers (fname, lname, email, username, password) VALUES
+        ('Joe', 'Shmo', 'jshmo@gmail.com', 'jshmo', sha1('shmo123')),
+        ('John', 'Doe', 'johndoe@gmail.com', 'jdoe', sha1('doe456'));
+
+ */
+
 class DataLayer
 {
+    private $_dbh;
+
+    /**
+     * DataLayer constructor.
+     * @param $dbh
+     */
+    function __construct($dbh)
+    {
+        $this->_dbh = $dbh;
+    }
+
+    /**
+     * insertMember inserts the members information into the database
+     */
+    function insertMember()
+    {
+        //get the member information
+        $member = $_SESSION['memberRank'];
+
+        //Define the query
+        $sql = "INSERT INTO primeusers (fname, lname, email, username, password) 
+	            VALUES (:fname, :lname, :age, :gender , :phone , :email ,:states ,:seeking ,:bio,:premium, :interests)";
+
+        //Prepare the statement
+        $statement = $this->_dbh->prepare($sql);
+
+        //check to see if a premiumMember and set the interests and the premiumCheck accordingly
+        if ($_SESSION['premiumMember']) {
+            $interests = $member->getIndoorInterests() . ", " . $member->getOutdoorInterests();
+            $premiumCheck = 1;
+        } else {
+            $interests = "";
+            $premiumCheck = 0;
+        }
+
+        //Bind the parameters
+        $statement->bindParam(':fname', $member->getFname(), PDO::PARAM_STR);
+        $statement->bindParam(':lname', $member->getLname(), PDO::PARAM_STR);
+        $statement->bindParam(':age', $member->getAge(), PDO::PARAM_INT);
+        $statement->bindParam(':gender', $member->getGender(), PDO::PARAM_STR);
+        $statement->bindParam(':phone', $member->getPhone(), PDO::PARAM_STR);
+        $statement->bindParam(':email', $member->getEmail(), PDO::PARAM_STR);
+        $statement->bindParam(':states', $member->getState(), PDO::PARAM_STR);
+        $statement->bindParam(':seeking', $member->getSeeking(), PDO::PARAM_STR);
+        $statement->bindParam(':bio', $member->getBio(), PDO::PARAM_STR);
+        $statement->bindParam(':premium', $premiumCheck, PDO::PARAM_INT);
+        $statement->bindParam(':interests', $interests, PDO::PARAM_STR);
+
+        //Execute
+        $statement->execute();
+
+    }
+
+    /**
+     * getMembers gets the members information from the database and orders by last name
+     * @return associative array
+     */
+    function getMembers()
+    {
+        //Define the query
+        $sql = "SELECT * FROM member ORDER BY lname";
+
+        //Prepare the statement
+        $statement = $this->_dbh->prepare($sql);
+
+        //Execute
+        $statement->execute();
+
+        //Get the results
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        return $result;
+    }
+
+    /**
+     * getMember($member_id) gets a single member from the database using their member id
+     * @param $member_id
+     * @return associative array
+     */
+    function getMember($member_id)
+    {
+        //Define the query
+        $sql = "SELECT * FROM member WHERE member_id = $member_id";
+
+        //Prepare the statement
+        $statement = $this->_dbh->prepare($sql);
+
+        //Execute
+        $statement->execute();
+
+        //Get the results
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        return $result;
+    }
+
+    /**
+     * getInterests($member_id) gets the interests from the member based on their member id
+     * @param $member_id
+     * @return associative array
+     */
+    function getInterests($member_id)
+    {
+        //Define the query
+        $sql = "SELECT interests FROM member WHERE member_id = $member_id";
+
+        //Prepare the statement
+        $statement = $this->_dbh->prepare($sql);
+
+        //Execute
+        $statement->execute();
+
+        //Get the results
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        return $result;
+    }
 
 }
